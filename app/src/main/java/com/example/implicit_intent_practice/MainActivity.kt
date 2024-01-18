e9ec6cd8c457c6e8fbf3ec3a87829d278acc98c7
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-18 12:08:25 +0530

    completed to learn implicit intent in android

--- app/src/main/java/com/example/implicit_intent_practice/MainActivity.kt
+++ app/src/main/java/com/example/implicit_intent_practice/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.implicit_intent_practice
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webButton = findViewById<CardView>(R.id.WebView)
+        val cameraButton = findViewById<CardView>(R.id.cameraView)
+        webButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.google.com/")
+            startActivity(intent)
+        }
+        cameraButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
     }
 }
